8337a38d8b9d874f3dc7793cb444667bad1e1d82
sourabh-devrukhkar <sourabh.dev9065@gmail.com>
2024-07-27 10:58:31 +0530

    robo_core discription changes

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -29,6 +29,18 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    #create a joint_state publisher node
+    node_joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',  
+    ) 
+
+    #create a rviz node
+    node_rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen'  
+    ) 
 
     # Launch!
     return LaunchDescription([
@@ -37,5 +49,7 @@ def generate_launch_description():
             default_value='false',
             description='Use sim time if true'),
 
-        node_robot_state_publisher
+        node_robot_state_publisher,
+        node_joint_state_publisher_gui,
+        node_rviz2
     ])
